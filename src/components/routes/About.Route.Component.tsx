cb8001938d6bffb7b8d1ab218e2c6b9b9f2a6294
Edward <edward.chopuryan@gmail.com>
2022-12-14 18:45:56 -0500

    style changes

--- src/components/routes/About.Route.Component.tsx
+++ src/components/routes/About.Route.Component.tsx
@@ -11,24 +11,34 @@ const AboutComponent = () => {
       <h1 className="pw-about-title">About me</h1>
       <div className="pw-about-content">
         <section className="pw-about-section-summary">
-          <p>Hi! I'm Edward, Software Engineer with 10+ experience</p>
-          <p>I design and build enterprise applications using modern technologies and cloud solutions</p>
+          <p>
+            Hi! I'm Edward, Software Engineer with 10+ experience.
+            <br />I design and build enterprise applications using modern technologies and cloud solutions
+          </p>
         </section>
         <div className="pw-about-mid-container">
           <section className="pw-about-section-exp">
-            <h3>Past Experience</h3>
-            <h4>Lead Software Engineer | 02/2019 - Present | EPAM systems | Boston, MA</h4>
-            <p>Full stack developer (.NET + React JS)</p>
-            <h4>Lead Software Engineer | 02/2019 - Present | EPAM systems | Boston, MA</h4>
-            <p>Full stack developer (.NET + React JS)</p>
+            <h2 className="pw-about-section-header">Past Experience</h2>
+            <div className="pw-about-section-content">
+              <h4>Lead Software Engineer | 02/2019 - Present | EPAM systems | Boston, MA</h4>
+              <p>Full stack developer (.NET + React JS)</p>
+              <h4>Sr Software Engineer | 01/2015 - 01/2019 | EPAM systems | Yerevan, Armenia</h4>
+              <p>.NET Developer</p>
+              <h4>Software Engineer | 04/2014 - 12/2014 | EPAM systems | Yerevan, Armenia</h4>
+              <p>Ruby on Rails backend developer</p>
+              <h4>Jr Software Engineer | 03/2012 - 02/2014 | Instigate CJSC | Yerevan, Armenia</h4>
+              <p>Creating and maintaining software for operating drones using C/C++</p>
+            </div>
           </section>
           <section className="pw-about-section-skills">
-            <h3>Skills</h3>
-            <p>
-              .NET, ReactJS, C#, JavaScript, TypeScript, SQL, Algorithms and Data Structures, TDD, BDD, DDD,,
-              Microservices, SOLID, Design Patterns, Agile, Scrum, Kanban, Machine Learning, PostgreSQL, SQL Server,
-              Oracle, Azure, AWS
-            </p>
+            <h2 className="pw-about-section-header">Skills</h2>
+            <div className="pw-about-section-content">
+              <p>
+                .NET, ReactJS, C#, JavaScript, TypeScript, SQL, Algorithms and Data Structures, TDD, BDD, DDD,,
+                Microservices, SOLID, Design Patterns, Agile, Scrum, Kanban, Machine Learning, PostgreSQL, SQL Server,
+                Oracle, Azure, AWS
+              </p>
+            </div>
           </section>
         </div>
       </div>
